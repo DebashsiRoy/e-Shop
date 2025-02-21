4c557486627cc0a392a3dee326c5ccb19fa90bc4
Debasish roy <97680393+woocom22@users.noreply.github.com>
2025-02-21 23:06:54 +0600

    define admin and user role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +13,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user-dashboard', [UserController::class, 'index'])->name('user-dashboard');
+});
+Route::middleware(['auth', AuthAdmin::class])->group(function () {
+    Route::get('/admin-dashboard', [AdminController::class, 'index'])->name('admin-dashboard');
+});
